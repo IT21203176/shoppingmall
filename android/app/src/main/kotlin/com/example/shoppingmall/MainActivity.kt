1617854a82ab6793945ebd6859eadf2dcca519c8
IT21203176 <it21203176@my.sliit.lk>
2023-09-04 16:19:51 +0530

    Janithya commit

--- android/app/src/main/kotlin/com/example/shoppingmall/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/shoppingmall/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.shoppingmall
 
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
+//import io.flutter.embedding.android.FlutterActivity
+//class MainActivity: FlutterActivity() {
+//}
